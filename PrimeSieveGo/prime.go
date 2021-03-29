0679289f1e9264611f58cf27f8820b398b93abf8
will.shaw <dasper@gmail.com>
2021-03-28 23:15:25 -0600

    adding flags and interface

--- PrimeSieveGo/prime.go
+++ PrimeSieveGo/prime.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math"
 	"time"
 )
 
+type PrimeInterface interface {
+	PrintResults(bool, time.Duration, int)
+	Run()
+	Set(int)
+}
+
+type primeBase struct {
+	size int
+	sqrt int
+}
+
 type primeBool struct {
-	size    int
-	sqrt    int
+	primeBase
 	isPrime []bool
+}
+
+type primeSieve struct {
+	primeBase
+	bits []uint8
 }
 
 var myDict = map[int]int{
@@ -24,12 +40,21 @@ var myDict = map[int]int{
 }
 
 func main() {
-	limit := 1000000
+	limit := flag.Int("l", 1000000, "limit")
+	useBool := flag.Bool("b", true, "use bool instead of bits")
+	flag.Parse()
+
+	var p PrimeInterface
 	passes := 0
 	start := time.Now()
-	p := primeBool{}
+	if *useBool {
+		p = &primeBool{}
+	} else {
+		p = &primeSieve{}
+	}
+
 	for time.Since(start).Seconds() < 10 {
-		p.Set(limit)
+		p.Set(*limit)
 		p.Run()
 		passes++
 	}
@@ -108,4 +133,95 @@ func (p *primeBool) countPrimes() int {
 		}
 	}
 	return count
+}
+
+func (p *primeSieve) getBit(index int) bool {
+	if index%2 == 0 {
+		return false
+	}
+	index = index / 2
+	return ((p.bits[index/8]) & (1 << (index % 8))) != 0
+}
+
+func (p *primeSieve) clearBit(index int) {
+	if index%2 == 0 {
+		fmt.Println("You're clearing even bits, which is sub-optimal.")
+		return
+	}
+	index = index / 2
+	p.bits[index/8] &^= (1 << (index % 8))
+}
+
+func (p *primeSieve) validateResults() bool {
+	if _, ok := myDict[p.size]; !ok {
+		return false
+	}
+	return myDict[p.size] == p.countPrimes()
+}
+
+func (p *primeSieve) Run() {
+	factor := 3
+
+	for factor < p.sqrt {
+		for num := factor; num < p.size; num += 2 {
+			if p.getBit(num) {
+				factor = num
+				break
+			}
+		}
+
+		for num := factor * 3; num < int(p.size); num += factor * 2 {
+			p.clearBit(num)
+		}
+
+		factor += 2
+	}
+}
+
+func (p *primeSieve) countPrimes() int {
+	count := 0
+	for i := 0; i < p.size; i += 1 {
+		if p.getBit(i) {
+			count += 1
+		}
+	}
+	return count
+}
+
+func (p *primeSieve) Set(size int) {
+	bits := make([]uint8, size/8+1)
+	for i := range bits {
+		bits[i] = 0xFF
+	}
+	p.bits = bits
+	p.size = size
+	p.sqrt = int(math.Sqrt(float64(p.size)))
+}
+
+func (p *primeSieve) PrintResults(showResults bool, duration time.Duration, passes int) {
+	if showResults {
+		fmt.Printf("2, ")
+	}
+
+	count := 1
+	for num := 3; num <= p.size; num += 1 {
+		if p.getBit(num) {
+			if showResults {
+				fmt.Printf("%d, ", num)
+			}
+			count++
+		}
+	}
+
+	if showResults {
+		fmt.Println()
+	}
+
+	fmt.Printf("Passes: %d, Time: %.6f, Avg: %.6f, Limit: %d, Count: %d, Valid: %v\n",
+		passes,
+		float64(duration)/float64(time.Second),
+		float64(duration)/float64(time.Second)/float64(passes),
+		p.size,
+		count,
+		p.validateResults())
 }
